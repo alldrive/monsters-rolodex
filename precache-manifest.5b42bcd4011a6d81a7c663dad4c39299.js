e1d2695b3ebbb9c76842e4ea6572fb8ed71b51b7
alldrive <57223233+alldrive@users.noreply.github.com>
2019-11-15 16:26:10 +0000

    Updates

---
+++ precache-manifest.5b42bcd4011a6d81a7c663dad4c39299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d81b88db167267b23870bf2fbc9741",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b99a52ccb02cb453103d",
+    "url": "/monsters-rolodex/static/css/main.ffe4f6d5.chunk.css"
+  },
+  {
+    "revision": "e021985d1d7e2a3dcdb4",
+    "url": "/monsters-rolodex/static/js/2.1fd7cad5.chunk.js"
+  },
+  {
+    "revision": "b99a52ccb02cb453103d",
+    "url": "/monsters-rolodex/static/js/main.9f34203b.chunk.js"
+  },
+  {
+    "revision": "c37f24c5eea30f677fd0",
+    "url": "/monsters-rolodex/static/js/runtime-main.1b782914.js"
+  }
+]);
